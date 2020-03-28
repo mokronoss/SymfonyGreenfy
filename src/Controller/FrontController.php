cdd13a3054b1d59c81113a1c48c681163b306635
Iza HP <you@example.com>
2020-03-28 22:37:18 +0100

    test front controller

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -7,13 +7,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
+
     /**
-     * @Route("/blog-details", name="blog-details")
+     * @Route("/index", name="index")
      */
-    public function blogDetails()
+    public function index()
     {
-        return $this->render('front/blog_details.html.twig');
+        return $this->render('front/index.html.twig');
     }
+    // /**
+    //  * @Route("/blog-details", name="blog-details")
+    //  */
+    // public function blogDetails()
+    // {
+    //     return $this->render('front/blog_details.html.twig');
+    // }
 
     /**
      * @Route("/blog", name="blog")
@@ -23,13 +31,13 @@ class FrontController extends AbstractController
         return $this->render('front/blog.html.twig');
     }
 
-    /**
-     * @Route("/check-out", name="check-out")
-     */
-    public function checkOut()
-    {
-        return $this->render('front/check_out.html.twig');
-    }
+    // /**
+    //  * @Route("/check-out", name="check-out")
+    //  */
+    // public function checkOut()
+    // {
+    //     return $this->render('front/check_out.html.twig');
+    // }
 
 
     /**
@@ -41,12 +49,52 @@ class FrontController extends AbstractController
     }
 
     /**
-     * @Route("/home", name="index")
+     * @Route("/faq", name="contact")
      */
-    public function index()
+    public function faq()
     {
-        return $this->render('front/index.html.twig');
+        return $this->render('front/faq.html.twig');
     }
 
 
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
+    }
+
+    /**
+     * @Route("/product", name="product")
+     */
+    public function product()
+    {
+        return $this->render('front/product.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/shop", name="shop")
+     */
+    public function shop()
+    {
+        return $this->render('front/shop.html.twig');
+    }
+
+    // /**
+    //  * @Route("/shopping-cart", name="shopping-cart")
+    //  */
+    
+    // public function shoppingCart()
+    // {
+    //     return $this->render('front/shopping_cart.html.twig');
+    // }
 }
